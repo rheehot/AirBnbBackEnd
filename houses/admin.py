2c504633e0cd98ddd67de06b43fd11aafb47f59f
thewoowon <thewoowon@naver.com>
2022-09-12 13:53:05 +0900

    20220912_AIRBNB

--- houses/admin.py
+++ houses/admin.py
@@ -9,6 +9,3 @@ class HouseAdmin(admin.ModelAdmin):
 
     list_display = ("name", "price_per_night", "address", "pets_allowed")
     list_filter = ("price_per_night","pets_allowed")
-    search_fields = ("address__startswith",)
-    list_display_links = ("name","address")
-    list_editable = ("pets_allowed",)
--- houses/models.py
+++ houses/models.py
@@ -12,7 +12,11 @@ class House(models.Model):
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(
         verbose_name="Pets Allowed",
-        default=True, help_text="Does this house allow pets?")
+        default=True,
+        help_text="Does this house allow pets?",
+    )
+    
+    owner = models.ForeignKey("users.User",on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
