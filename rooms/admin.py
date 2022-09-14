6e9992a230057aa85ee072331f53f719f1791f8e
thewoowon <thewoowon@naver.com>
2022-09-14 23:47:15 +0900

    20220914_AIRBNB

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Room, Amenity
 
-# Register your models here.
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    pass
+    # list_display = (
+    #     "name",
+    #     "country",
+    #     "city",
+    #     "price",
+    #     "rooms",
+    #     "toilets",
+    #     "description",
+    #     "address",
+    #     "pet_friendly",
+    #     "kind",
+    #     "owner",
+    # )
+    # list_filter = ("country", "city", "price", "rooms", "toilets", "kind", "owner",)
+    # search_fields = ("^city", "^country",)
+    
+@admin.register(Amenity)    
+class AmenityAdmin(admin.ModelAdmin):
+    pass
+    # list_display = ("name", "description",)
+    # search_fields = ("^name",)
