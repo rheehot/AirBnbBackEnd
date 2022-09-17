cb3a556c05382f8f5d2c623e3671633305e45af9
thewoowon <thewoowon@naver.com>
2022-09-18 00:30:27 +0900

    20220918_AIRBNB

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ CUSTOM_APPS = [
     "reviews.apps.ReviewsConfig",
     "wishlists.apps.WishlistsConfig",
     "bookings.apps.BookingsConfig",
+    "medias.apps.MediasConfig",
 ]
 
 # Application definition
